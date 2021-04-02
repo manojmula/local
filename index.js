ef1d9e3a928dccf97d7f6c73796d3fa2fae0194b
Manoj <manugwt@gmail.com>
2021-04-02 15:38:38 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,95 @@
+//Bangla
+var bangla = document.getElementById("banglaVoice")
+
+function playBangla(){
+    bangla.play();
+}
+
+//English
+var english = document.getElementById("englishVoice")
+
+function playEnglish()
+{
+    english.play();
+}
+
+//Gujarathi
+var gujarati = document.getElementById("GujarathiVoice")
+
+function playGujarathi()
+{
+    gujarati.play();
+}
+
+//Hindi
+var hindi = document.getElementById("hindiVoice")
+
+function playHindi()
+{
+    hindi.play();
+}
+
+
+//Kannada
+var kannada = document.getElementById("kannadaVoice")
+
+function playKannda()
+{
+    kannada.play();
+}
+
+//Malayalam
+var malayalam = document.getElementById("malayalamVoice")
+
+function playMalayalam()
+{
+    malayalam.play();
+}
+
+//Marathi
+var marathi = document.getElementById("marathiVoice")
+
+function playMarathi()
+{
+    marathi.play();
+}
+
+//Odia
+var odia = document.getElementById("odiaVoice")
+
+function playOdia()
+{
+    odia.play();
+}
+
+//Panjabi
+var panjabi = document.getElementById("panjabihVoice")
+
+function playPanjabi()
+{
+    panjabi.play();
+}
+
+//Tamil
+var tamil = document.getElementById("tamilVoice")
+
+function playTamil()
+{
+    tamil.play();
+}
+
+//Telugu
+var telugu = document.getElementById("teluguVoice")
+
+function playTelugu()
+{
+    telugu.play();
+}
+
+//Urdu
+var urdu = document.getElementById("urduVoice")
+
+function playUrdu()
+{
+    urdu.play();
+}
